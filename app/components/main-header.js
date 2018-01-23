1d603b7784cf813e27f3c3fd41d177de619d2c3e
iirenesg <irenepsg10@gmail.com>
2018-01-23 05:07:47 -0600

    Mobile menu functionality

--- app/components/main-header.js
+++ app/components/main-header.js
@@ -3,6 +3,23 @@ import Component from '@ember/component';
 export default Component.extend({
   classNameBindings: ['onScroll:header-mini'],
   onScroll: false,
+  menuOpen: false,
+
+  actions: {
+    showMenu() {
+      $('#mobile-nav').toggleClass('nav-mobile-container-open');
+      $('body').toggleClass('push-body');
+      let time = 400;
+      if(this.menuOpen) time = 100;
+
+      setTimeout(() => {
+        $('#mobile-bg').toggleClass('mobile-bg-shown');
+        $('#close-menu-btn').toggleClass('icon-menu-close-shown');
+        this.menuOpen = !this.menuOpen;
+      }, time)
+
+    }
+  },
 
   didInsertElement() {
     let headerComponent = this;
