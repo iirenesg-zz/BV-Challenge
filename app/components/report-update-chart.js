7af9d29dd32de7a94ce3060ba1b044bb7461d73a
iirenesg <irenepsg10@gmail.com>
2018-01-23 05:23:16 -0600

    Canvas functionality - Update report and Risk score charts

--- app/components/report-update-chart.js
+++ app/components/report-update-chart.js
@@ -8,29 +8,41 @@ export default Component.extend({
     let self = this;
     let parent = $('#report-update-container')[0];
     let canvas = $('#report-update-canvas')[0];
-    let strokeWidth = 15;
-    if(window.innerWidth > 1024) strokeWidth += 5;
-    canvas.width = parent.getBoundingClientRect().width;
-    canvas.height = parent.getBoundingClientRect().height;
-
     let ctx = canvas.getContext("2d");
-    ctx.lineWidth = strokeWidth;
 
+    let strokeWidth = 15;
     let startAngle = 3*Math.PI/4;
     let endAngle = 9*Math.PI/4;
-    let radius = (canvas.width/2) - 35; 
-    if(window.innerWidth >= 768 && window.innerWidth < 1200) radius += 10;
 
-    ctx.beginPath();
-    ctx.arc(canvas.width/2, canvas.height/2, radius, startAngle, endAngle);
-    ctx.strokeStyle = 'rgba(255,255,255,0.48)';
-    ctx.stroke();
+    function draw() {
+      if(window.innerWidth > 1024) strokeWidth = 20;
+      canvas.width = parent.getBoundingClientRect().width;
+      canvas.height = parent.getBoundingClientRect().height;
+      
+      let centerX = canvas.width/2;
+      let centerY = canvas.height/2;
+      let radius = centerX - 35; 
+      if(window.innerWidth < 480 || window.innerWidth >= 768 && window.innerWidth < 1200) radius += 10;
 
-    ctx.beginPath();
-    let percent = self.amtCurrent/self.amtTotal;
-    let currentAngle = ((7*Math.PI/4)*percent) + startAngle;
-    ctx.arc(canvas.width/2, canvas.height/2, radius, startAngle, currentAngle);
-    ctx.strokeStyle = 'rgba(255,255,255,1)';
-    ctx.stroke();
+      ctx.lineWidth = strokeWidth;
+
+      ctx.beginPath();
+      ctx.arc(centerX, centerY, radius, startAngle, endAngle);
+      ctx.strokeStyle = 'rgba(255,255,255,0.48)';
+      ctx.stroke();
+
+      ctx.beginPath();
+      let percent = self.amtCurrent/self.amtTotal;
+      let currentAngle = ((7*Math.PI/4)*percent) + startAngle;
+      ctx.arc(centerX, centerY, radius, startAngle, currentAngle);
+      ctx.strokeStyle = '#FFF';
+      ctx.stroke();
+    }
+
+    draw();
+
+    Ember.$(window).resize(() => {
+      draw();
+    });
   }
 });
--- app/components/risk-score-chart.js
+++ app/components/risk-score-chart.js
@@ -13,40 +13,55 @@ export default Component.extend({
     let self = this;
     let parent = $('#risk-score-container')[0];
     let canvas = $('#risk-score-canvas')[0];
-    canvas.width = parent.getBoundingClientRect().width;
-    canvas.height = parent.getBoundingClientRect().width;
 
     let startAngle = 3*Math.PI/4;
     let endAngle = 6*Math.PI/4;
     let lastAngle = startAngle;
     let riskAngle = (endAngle * self.currentRisk) + startAngle;
-    let radius = (canvas.width/2) - 20;
 
     var ctx = canvas.getContext("2d");
-    ctx.lineWidth = 20;
 
-    self.values.forEach((v) => {
+    function draw() {
+      canvas.width = parent.getBoundingClientRect().width;
+      canvas.height = parent.getBoundingClientRect().width;   
+  
+      let centerX = canvas.width/2;
+      let centerY = canvas.height/2;
+      let radius = centerX - 20;
+
+      ctx.lineWidth = 20;
+
+      self.values.forEach((v) => {
+        ctx.beginPath();
+        let currentAngle = (endAngle * v.value) + lastAngle;
+        ctx.arc(centerX, centerY, radius, lastAngle, currentAngle);
+        ctx.strokeStyle = v.color;
+        ctx.stroke();
+        lastAngle = currentAngle;
+      })
+
       ctx.beginPath();
-      let currentAngle = (endAngle * v.value) + lastAngle;
-      ctx.arc(canvas.width/2, canvas.height/2, radius, lastAngle, currentAngle);
-      ctx.strokeStyle = v.color;
+      ctx.strokeStyle ='rgba(0, 2, 69, 0.64)';
+      ctx.arc(centerX, centerY, radius, riskAngle, Math.PI/2);
       ctx.stroke();
-      lastAngle = currentAngle;
-    })
 
-    ctx.beginPath();
-    ctx.strokeStyle ='rgba(0, 2, 69, 0.64)';
-    ctx.arc(canvas.width/2, canvas.height/2, radius, riskAngle, Math.PI/2);
-    ctx.stroke();
+      ctx.lineWidth = 10;
+      ctx.lineCap="round";
+      ctx.beginPath();
+      let x = Math.cos(riskAngle);
+      let y = Math.sin(riskAngle);
+      ctx.moveTo((radius - 20) * (x + centerX), (radius - 20) * (y + centerY));
+      ctx.lineTo((radius + 20) * (x + centerX), (radius + 20) * (y + centerY));
+      ctx.strokeStyle = '#FFF';
+      ctx.stroke();
 
-    ctx.lineWidth = 10;
-    ctx.lineCap="round";
-    ctx.beginPath();
-    ctx.moveTo((radius - 20) * (Math.cos(riskAngle)) + canvas.width/2, (radius - 20) * (Math.sin(riskAngle)) + canvas.height/2);
-    ctx.lineTo((radius + 20) * (Math.cos(riskAngle)) + canvas.width/2, (radius + 20) * (Math.sin(riskAngle)) + canvas.height/2);
-    ctx.strokeStyle = '#FFF';
-    ctx.stroke();
+      lastAngle = startAngle;
+    }
 
+    draw();
 
+    Ember.$(window).resize(() => {
+      draw();
+    });
   }
 });
